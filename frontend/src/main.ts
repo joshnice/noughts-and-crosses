71640817cd72a80ff6df696ccd4d48b0631319a6
joshnice <joshliamnice@gmail.com>
2025-05-01 20:26:40 +0100

    Added primevue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,14 @@
+import Aura from "@primeuix/themes/aura";
+import PrimeVue from "primevue/config";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./routing/routing";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.use(PrimeVue, {
+	theme: {
+		preset: Aura,
+	},
+});
+app.mount("#app");
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <script setup>
+import { ref } from "vue";
+
 console.log("Home page has mounted");
+const showCreateNewgGameModal = ref(false);
+
+function createNewGame() {
+	showCreateNewgGameModal.value = !showCreateNewgGameModal.value;
+}
 </script>
 
 <template>
   <h1>Home Page</h1>
+  <button @click="createNewGame">Create new game</button>
+  <div v-if="showCreateNewgGameModal">
+      Create a new game
+  </div>
 </template>
